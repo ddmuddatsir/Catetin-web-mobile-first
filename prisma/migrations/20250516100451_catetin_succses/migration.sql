6b5f7f4dcc92c34e2f23bcd2bc89cbc9639dcacf
Nur Muhammad Muddatsir <dedemudasir@gmail.com>
2025-08-20 11:59:19 +0700

    fix error on deploy delete prisma client and full firebase

--- prisma/migrations/20250516100451_catetin_succses/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `createAr` on the `Transaction` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Transaction" DROP COLUMN "createAr",
-ADD COLUMN     "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
